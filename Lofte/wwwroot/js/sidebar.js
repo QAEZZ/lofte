3db92d78e9c5b8c9a287ac66e775ba0bdeadff1b
QAEZZ <russian.devv@gmail.com>
2024-02-29 14:20:10 -0600

    restructured CSS

--- Lofte/wwwroot/js/sidebar.js
+++ Lofte/wwwroot/js/sidebar.js
@@ -6,10 +6,13 @@ sidebar_toggle.addEventListener('click', function () {
     sidebar_resize.classList.toggle('hidden');
 
     if (sidebar.classList.contains('collapsed')) {
-        sidebar.style.width = 'max-content';
+        sidebar.style.width = '200px'; // same as in css/sidebar.css
         sidebar.classList.remove('collapsed');
     } else {
         sidebar.style.width = '0';
         sidebar.classList.add('collapsed');
     }
 });
+
+// alert(sidebar.getBoundingClientRect().width);
+// sidebar.style.width = sidebar.getBoundingClientRect().width + 1 + 'px';
